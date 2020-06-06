9071d219112f768ef7b911069ff24d9f37ebcc24
Luckycn <66270214+Luckycn@users.noreply.github.com>
2020-06-06 10:55:09 +0800

    Update app-v3.js

--- app-v3.js
+++ app-v3.js
@@ -6,13 +6,13 @@
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
 // 在head 中 加载 必要静态
-document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.3/css/mdui_v2.min.css">');
-document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.6/css/nexmoe_v2.min.css">');
-document.write('<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.3/js/mdui.min.js"></script>');
-document.write('<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.5/js/flv.min.js"></script>');
-document.write('<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.5/js/DPlayer.min.js"></script>');
+document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe@1.1.3/css/mdui_v2.min.css">');
+document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe@1.1.6/css/nexmoe_v2.min.css">');
+document.write('<script src="//cdn.jsdelivr.net/gh/Luckycn/Luckycn/GoIndex-theme-nexmoe@1.1.3/js/mdui.min.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe@1.1.5/js/flv.min.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe@1.1.5/js/DPlayer.min.js"></script>');
 // markdown支持
-document.write('<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.3/js/markdown-it.min.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe@1.1.3/js/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>');
 // add custome theme and darkmode
 if (UI.dark_mode) {
@@ -26,10 +26,10 @@ function init() {
   var html = `
 <header class="nexmoe-nav">
 	<div class="navSize">
-			<a href="/"><img class="avatar" src="https://i.loli.net/2020/04/24/wCxMZFtiv6aKmQ1.png"/></a>
+			<a href="/"><img class="avatar" src="https://gitee.com/Unkaer/blog/raw/master/images/portrait.webp"/></a>
 			<div class="nav_menu">
 				<ul class="menu_ul">
-					<li class="menu_li"><a href="https://5mayrain.github.io/" target="_blank">博客</a></li>
+					<li class="menu_li"><a href="/" target="_blank">博客</a></li>
 					<li class="menu_li"><a href="https://drive.google.com/" target="_blank">登陆</a></li>
 				</ul>
 				<div class="nav_icon" ></div>
@@ -744,8 +744,8 @@ function file_code(path) {
 </div>
 <a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
-<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/ace.js"></script>
-<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/ext-language_tools.js"></script>
+<script src="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe/js/ace.js"></script>
+<script src="//cdn.jsdelivr.net/gh/Luckycn/GoIndex-theme-nexmoe/js/ext-language_tools.js"></script>
 	`;
   $('#content').html(content);
 
